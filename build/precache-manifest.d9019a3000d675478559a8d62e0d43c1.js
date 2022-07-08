653e29949f49ffca3c6c96aaf84550e6ae8e35ee
Sonukum226 <sonukum870@gmail.com>
2022-07-08 17:22:45 +0530

    done

---
+++ build/precache-manifest.d9019a3000d675478559a8d62e0d43c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee97b0b5174e36cb6d920a4dc5fb543c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03472130b4da43b8ba30",
+    "url": "/static/css/2.7b4682b0.chunk.css"
+  },
+  {
+    "revision": "c99aa323e23bd47237f7",
+    "url": "/static/css/main.2bf9b0f6.chunk.css"
+  },
+  {
+    "revision": "03472130b4da43b8ba30",
+    "url": "/static/js/2.54d6c914.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.54d6c914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99aa323e23bd47237f7",
+    "url": "/static/js/main.e36ecf51.chunk.js"
+  },
+  {
+    "revision": "a752674f1df1a21c9e43",
+    "url": "/static/js/runtime-main.6e376787.js"
+  }
+]);
