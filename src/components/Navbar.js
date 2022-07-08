62c09db1d0b814ecdea93848f9a2c57e66e8ce91
Sonukum226 <sonukum870@gmail.com>
2022-07-08 17:05:55 +0530

    done

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <a className="navbar-brand" href="#">
+          PARKING MANAGEMENT SYSTEM
+        </a>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
